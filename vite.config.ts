52c67da9b2881868a79c8067226597c91abde0b5
ivory <8841504+ivory_tower1026@user.noreply.gitee.com>
2026-02-14 15:14:23 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8002',
-        changeOrigin: true
-      },
-      '/dav': {
-        target: 'http://localhost:8002',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      vue(),
+      vuetify({ autoImport: true })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: parseInt(env.VITE_SERVER_PORT || '3000'),
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:8002',
+          changeOrigin: true
+        },
+        '/dav': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:8002',
+          changeOrigin: true
+        }
       }
     }
   }
